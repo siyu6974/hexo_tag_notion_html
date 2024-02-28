625b9507129c372843b08bcef59a183aaa9e7863
Siyu Zhang <astro6974@gmail.com>
2024-02-27 23:55:15 -0800

    Use regex to remove media CSS, add fancy box to images

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 'use strict';
 
-hexo.extend.tag.register('notion_html', function(args, content) {
-  // Regular expression to match @media section
-  let regex = /@media\s.*?\{[\s\S]*?\}/g;
+function setupFancyBox(content) {
+  let regex = /<body[^>]*>/i;
 
-  let content = content.replace(regex, '');
+  // Content to insert
+  let scriptBlock = `
+  <script>
+    Fancybox.bind('[data-fancybox]', {
+      // Your custom options
+    });
+  </script>
+  `;
+  return content.replace(regex, '$&\n' + scriptBlock);
+}
+
+hexo.extend.tag.register('notion_html', function (args, content) {
+  // Regular expression to match @media section
+  var regex = /@media\s.*?\{[\s\S]*?\}/g;
+  content = content.replaceAll(regex, '');
+
+  // Regex pattern to match <a> tags with <img> tag inside
+  regex = /<a\s+([^>]*)href="([^"]*)"(?=[^>]*>\s*<img)/g;
+  // Replace <a> tag with data-fancybox attribute added
+  content = content.replaceAll(regex, '<a $1 data-fancybox href="$2"');
+  console.log(content);
+  content = setupFancyBox(content);
   return content;
 
-}, {ends: true});
+}, { ends: true });
